c3c42a731ad8e1c0d96f224eb48dc3b1f84d61a8
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-01-28 00:09:22 +0530

    Create a function to dynamically set grid template rows and columns based on board size. Add code to generate a grid of yellow divs within the board on DOMContentLoaded.

--- main.js
+++ main.js
@@ -7,4 +7,12 @@ function createBoard(size){
 
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
+    let numDivs = size*size;
+
+    for(let i=0; i<numDivs; i++){
+        let div = document.createElement("div");
+        div.style.backgroundColor="yellow";
+        board.insertAdjacentElement("beforeend", div);
+    }
 }
